3260156482d69f63ffb53a8d36c2a65a2bd62254
IamKikin <85178201+iamkikin-off@users.noreply.github.com>
2024-11-09 19:37:30 -0800

    Update main.gd

--- Godot/TitleAPI/main.gd
+++ Godot/TitleAPI/main.gd
@@ -1,25 +1,40 @@
 extends Node
 
-var titles = {
-	# 76561199157842767: "[color=#D57EEC][KIKIN][/color]",
-	76561198123766303: "[color=#913BED][LOAF][/color]"
-}
+var plr_titles = {} # The dictionary where titles go.
+var prefix = "[TitleAPI] " # Prefix of the mod
 
 func _ready():
-	var YourSteamID = Network.STEAM_ID
 	
-	print("[Kikin's Title API] Loaded! - " + str(YourSteamID))
-	register_title(76561199157842767, "[color=#D57EEC][KIKIN][/color]")
+	# Register creator titles.
+	register_creator_titles()
 	
+	# -- DEBUG --
+	print(prefix + "Loaded!")
+	# -- /DEBUG --
 
 func register_title(id, title):
-	titles[id] = title
+	plr_titles[id] = title
+	
+	# -- DEBUG --
+	print(prefix + "Successfully set title to: " + title + ", for: " + str(id))
+	print(prefix + id + " has a title: " + plr_titles[id])
+	# -- /DEBUG --
+
+func register_creator_titles():
+	
+	# Register the creator titles.
+	register_title(76561199157842767, "[rainbow][KIKIN][/rainbow]") # [IAMKIKIN] - IamKikin - Purple
+	register_title(76561198123766303, "[color=#913BED][LOAF][/color]") # [LOAF] - Nowaha - Purple
+	
+	print(prefix + "Successfully set creator titles!")
 	
 func resolve_name(player_id, name):
-	print(player_id)
-	print(titles.get(player_id))
-	var title = titles.get(player_id)
-	if title == null: 
+	var players_title = plr_titles.get(player_id)
+	if players_title == null: 
+		# If player doesn't have a title, return just their name.
 		return name
-	return title + "\n" + name
+	#If player has a title, then return it from the dictionary.
+	print(prefix + "Successfully resolved.")
+	return players_title + "\n" + name
+		
 		
