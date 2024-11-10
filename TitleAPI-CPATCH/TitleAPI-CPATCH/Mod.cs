db89a192b3e708d46cd92c60bd1a74af5ad34d4e
IamKikin <85178201+iamkikin-off@users.noreply.github.com>
2024-11-10 11:58:52 -0800

    Add files via upload

---
+++ TitleAPI-CPATCH/TitleAPI-CPATCH/Mod.cs
@@ -0,0 +1,18 @@
+﻿using GDWeave;
+using TitleAPI_CPATCH;
+
+namespace TitleAPI_CPATCH;
+
+public class Mod : IMod {
+    public Config Config;
+
+    public Mod(IModInterface modInterface) {
+        this.Config = modInterface.ReadConfig<Config>();
+        modInterface.RegisterScriptMod(new Main());
+        modInterface.Logger.Information("HOT DOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOGS!");
+    }
+
+    public void Dispose() {
+        // Cleanup anything you do here
+    }
+}
