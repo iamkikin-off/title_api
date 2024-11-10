47ce33a5a86eacac78b7ab827e4e43e9169d3728
Noah Verkaik <noahverkaik@gmail.com>
2024-11-10 21:10:48 +0100

    Add get_title & deregister_title

--- Godot/TitleAPI/main.gd
+++ Godot/TitleAPI/main.gd
@@ -4,14 +4,18 @@ var plr_titles = {} # The dictionary where titles go.
 var prefix = "[TitleAPI] " # Prefix of the mod
 
 func _ready():
-	
 	# Register creator titles.
-	register_creator_titles()
+	_register_creator_titles()
 	
 	# -- DEBUG --
 	print(prefix + "Loaded!")
 	# -- /DEBUG --
 
+# Get the title of a player. Returns null if they have no custom one.
+func get_title(player_id):
+	return plr_titles.get(player_id)
+
+# Register a new title for a player
 func register_title(id, title):
 	plr_titles[id] = title
 	
@@ -20,21 +24,25 @@ func register_title(id, title):
 	print(prefix + "" + str(id) + " has a title: " + plr_titles[id])
 	# -- /DEBUG --
 
-func register_creator_titles():
+func deregister_title(player_id):
+	plr_titles.erase(player_id)
 	
-	# Register the creator titles.
-	register_title(76561199157842767, "[rainbow][KIKIN][/rainbow]") # [IAMKIKIN] - IamKikin - Purple
-	register_title(76561198123766303, "[color=#913BED][LOAF][/color]") # [LOAF] - Nowaha - Purple
-	
-	print(prefix + "Successfully set creator titles!")
-	
+	# -- DEBUG --
+	print(prefix + "Successfully removed title for: " + str(player_id))
+	# -- /DEBUG --
+
 func resolve_name(player_id, name):
 	var players_title = plr_titles.get(player_id)
 	if players_title == null: 
 		# If player doesn't have a title, return just their name.
 		return name
-	#If player has a title, then return it from the dictionary.
+	
+	# If player has a title, then return it from the dictionary.
 	print(prefix + "Successfully resolved.")
 	return players_title + "\n" + name
 		
-		
+func _register_creator_titles():
+	register_title(76561199157842767, "[rainbow][KIKIN][/rainbow]") # [IAMKIKIN] - IamKikin - Purple
+	register_title(76561198123766303, "[color=#913BED][LOAF][/color]") # [LOAF] - Nowaha - Purple
+	
+	print(prefix + "Successfully set creator titles!")
